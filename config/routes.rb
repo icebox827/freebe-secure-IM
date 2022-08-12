995a5fb229c5750ed6a4eff1e3b28a79ab97308b
Denis Lafontant <icebox827@gmail.com>
2022-08-12 13:21:49 -0400

    Install semantic-ui

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'sessions/index'
   get 'chatrooms/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'chatroom#index'
-  get 'login', to: 'session#new'
+  root 'chatrooms#index'
+  get 'login', to: 'sessions#new'
 end
