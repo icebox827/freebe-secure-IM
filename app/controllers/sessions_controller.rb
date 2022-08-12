81f396ba608559df8dd8d7f0f4ac265ceba5e9a0
Denis Lafontant <icebox827@gmail.com>
2022-08-12 14:28:54 -0400

    Update session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,22 @@
 class SessionsController < ApplicationController
-  def index; end
+  def new; end
+
+  def create
+    @user = User.find_by(phone: params[:session][:user])
+
+    if @user
+      session[:user_id] = @user.id
+      session[:username] = @user.name
+      redirect_to chatrooms_path
+    else
+      flash[:alert] = 'Wrong login info'
+      render 'new'
+    end
+  end
+
+  def destroy
+    reset_session
+    flash[:success] = 'Log out successfully'
+    redirect_to root_path
+  end
 end
