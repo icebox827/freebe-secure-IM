a2a07e447123ac52d6cfcc0b5ac88d57837bc51e
Denis Lafontant <icebox827@gmail.com>
2022-08-12 16:17:08 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'sessions#new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[new create show]
   resources :chatrooms
+  resources :sessions, only: %i[new create destroy]
+  scope :sessions do
+    get 'sign_out' => 'sessions#destroy'
+  end
 end
