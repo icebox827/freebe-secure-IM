a46b1f9fe732b90d0d7087292e12b43761382605
Denis Lafontant <icebox827@gmail.com>
2022-08-15 13:24:52 -0400

    Update routes for new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'sessions#new'
+  get 'signup', to: 'users#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users, only: %i[index show create edit update destroy]
   resources :chatrooms
 end
